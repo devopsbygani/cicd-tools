9228f57cd8321db909eba863f43b7c0c7afe807c
ganeshmoka <ganeshmoka1999@gmail.com>
2025-07-10 22:03:23 +0530

    cicd

--- main.tf
+++ main.tf
@@ -1,43 +1,56 @@
 module "jenkins" {
   source  = "terraform-aws-modules/ec2-instance/aws"  #open source module for instance creation.
-  name = var.servers[0]
+  name = "jenkins"
   ami = data.aws_ami.joindevops.id
   instance_type          = "t3.small"
   vpc_security_group_ids = [var.sg_id]
+  subnet_id = "subnet-04269c5138b0e91e6" #replace your Subnet
   user_data = file("jenkins.sh")
   tags = merge(var.common_tags,var.server_tags,
   {
-    Name = var.servers[0]
+    Name = "jenkins"
   }
   )
-  root_block_device = 
-    {
-    volume_size = 50       # Size of the root volume in GB
-    volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
-    delete_on_termination = true  # Automatically delete the volume when the instance is terminated
-    }
+  # root_block_device = [
+  #   {
+  #   volume_size = 50       # Size of the root volume in GB
+  #   volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
+  #   delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+  #   }
+  # ]
+  root_block_device = {
+  size                  = 50
+  type                  = "gp3"
+  delete_on_termination = true
+}
   
 }
 
 module "jenkins_agent" {
   source  = "terraform-aws-modules/ec2-instance/aws"  #open source module for instance creation.
-  name = var.servers[1]
+  name = "jenkins-agent"
   ami = data.aws_ami.joindevops.id
   instance_type          = "t3.small"
   vpc_security_group_ids = [var.sg_id]
+  subnet_id = "subnet-04269c5138b0e91e6" #replace your default sSubnet 
   user_data = file("jenkins_agent.sh")
   tags = merge(var.common_tags,var.server_tags,
   {
-    Name = var.servers[1]
+    Name = "jenkins-agent"
   }
   )
-  root_block_device = 
-    {
-    volume_size = 50       # Size of the root volume in GB
-    volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
-    delete_on_termination = true  # Automatically delete the volume when the instance is terminated
-    }
-   
+  # root_block_device = [
+  #   {
+  #   volume_size = 50       # Size of the root volume in GB
+  #   volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
+  #   delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+  #   }
+  # ]
+  root_block_device = {
+  size                  = 50
+  type                  = "gp3"
+  delete_on_termination = true
+}
 }
 
 # module "jenkins_nodejs" {
