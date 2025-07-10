d44704e0ba5fbba2c0d33cb48273831e89a30fc8
ganeshmoka <ganeshmoka1999@gmail.com>
2025-07-10 19:36:46 +0530

    version to 6.0

--- main.tf
+++ main.tf
@@ -10,13 +10,13 @@ module "jenkins" {
     Name = var.servers[0]
   }
   )
-  root_block_device = [
+  root_block_device = {
     {
     volume_size = 50       # Size of the root volume in GB
     volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
     delete_on_termination = true  # Automatically delete the volume when the instance is terminated
     }
-  ]
+  }
 }
 
 module "jenkins_agent" {
@@ -31,13 +31,13 @@ module "jenkins_agent" {
     Name = var.servers[1]
   }
   )
-  root_block_device = [
+  root_block_device = {
     {
     volume_size = 50       # Size of the root volume in GB
     volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
     delete_on_termination = true  # Automatically delete the volume when the instance is terminated
     }
-  ]  
+  }  
 }
 
 # module "jenkins_nodejs" {
