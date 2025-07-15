aec3ffd5f5facf34494c02e84364f1b2534add89
ganeshmoka <ganeshmoka1999@gmail.com>
2025-07-15 22:29:14 +0530

    jenkins

--- main.tf
+++ main.tf
@@ -26,32 +26,32 @@ module "jenkins" {
   
 }
 
-# module "jenkins_agent" {
-#   source  = "terraform-aws-modules/ec2-instance/aws"  #open source module for instance creation.
-#   name = "jenkins-agent"
-#   ami = data.aws_ami.joindevops.id
-#   instance_type          = "t3.small"
-#   vpc_security_group_ids = [var.sg_id]
-#   subnet_id = "subnet-04269c5138b0e91e6" #replace your default sSubnet 
-#   user_data = file("jenkins_agent.sh")
-#   tags = merge(var.common_tags,var.server_tags,
-#   {
-#     Name = "jenkins-agent"
-#   }
-#   )
-#   # root_block_device = [
-#   #   {
-#   #   volume_size = 50       # Size of the root volume in GB
-#   #   volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
-#   #   delete_on_termination = true  # Automatically delete the volume when the instance is terminated
-#   #   }
-#   # ]
-#   root_block_device = {
-#   size                  = 50
-#   type                  = "gp3"
-#   delete_on_termination = true
-# }
-# }
+module "jenkins_agent" {
+  source  = "terraform-aws-modules/ec2-instance/aws"  #open source module for instance creation.
+  name = "jenkins-agent"
+  ami = data.aws_ami.joindevops.id
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [var.sg_id]
+  subnet_id = "subnet-04269c5138b0e91e6" #replace your default sSubnet 
+  user_data = file("jenkins_agent.sh")
+  tags = merge(var.common_tags,var.server_tags,
+  {
+    Name = "jenkins-agent"
+  }
+  )
+  # root_block_device = [
+  #   {
+  #   volume_size = 50       # Size of the root volume in GB
+  #   volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
+  #   delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+  #   }
+  # ]
+  root_block_device = {
+  size                  = 50
+  type                  = "gp3"
+  delete_on_termination = true
+}
+}
 
 # module "jenkins_nodejs" {
 #   source  = "terraform-aws-modules/ec2-instance/aws"  #open source module for instance creation.
@@ -90,18 +90,18 @@ module "records" {
       ]
       allow_overwrite = true
       
+    },
+
+    {
+      name    = "jenkins-agent" # jenkins-agent.devgani.online
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.jenkins_agent.private_ip
+      ]
+      allow_overwrite = true
+
     }
-
-    # {
-    #   name    = "jenkins-agent" # jenkins-agent.devgani.online
-    #   type    = "A"
-    #   ttl     = 1
-    #   records = [
-    #     module.jenkins_agent.private_ip
-    #   ]
-    #   allow_overwrite = true
-
-    # }
 
     # {
     #   name    = "nodejseks" # nodejseks.devgani.online
